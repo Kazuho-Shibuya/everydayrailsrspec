1a2536d59470111d5bd2d24fd95e3ce4bbee54d9
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-08 18:32:41 +0900

    コールバック

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -24,6 +24,12 @@ RSpec.describe Project, type: :model do
     end
   end
 
+  # たくさんのメモが付いていること
+  it 'can have many notes' do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it 'does not allow duplicate project names per user' do
     user = User.create!(
