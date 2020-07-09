b2606afb8af1818c6341eecef4c7545d1d59d04b
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-09 20:38:09 +0900

    サポートモジュール

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -6,31 +6,26 @@ RSpec.feature 'Projects', type: :feature do
   # ユーザーは新しいプロジェクトを作成する
   scenario 'user creates a new project' do
     user = FactoryBot.create(:user)
+    sign_in user
+
     visit root_path
-    click_link 'Sign in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
 
     expect do
       click_link 'New Project'
       fill_in 'Name', with: 'Test Project'
       fill_in 'Description', with: 'Trying out Capybara'
       click_button 'Create Project'
-      expect(page).to have_content 'Project was successfully created'
-      expect(page).to have_content 'Test Project'
-      expect(page).to have_content "Owner: #{user.name}"
     end.to change(user.projects, :count).by(1)
+
+    expect(page).to have_content 'Project was successfully created'
+    expect(page).to have_content 'Test Project'
+    expect(page).to have_content "Owner: #{user.name}"
   end
 
   # ゲストがプロジェクトを追加する
   scenario 'guest adds a project' do
     user = FactoryBot.create(:user)
-    visit root_path
-    click_link 'Sign in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
+    sign_in user
     visit projects_path
     click_link 'New Project'
   end
