64e959ccb6fa18c3effb325036ce666cd78e327d
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-11 19:21:02 +0900

    バックグラウンドワーカーのテスト

---
+++ spec/jobs/geocode_user_job_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GeocodeUserJob, type: :job do
+  # user の geocode を呼ぶこと
+  it 'calls geocode on the user' do
+    user = instance_double('User')
+    expect(user).to receive(:geocode)
+    GeocodeUserJob.perform_now(user)
+  end
+end
